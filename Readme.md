b91a72cbd5bd1104a854415d06354c3a9bd53f54
Jorge <jorgejuniorhodu@gmial.com>
2023-09-02 16:31:07 -0300

    Projeto final

--- Readme.md
+++ Readme.md
@@ -1,2 +1,10 @@
 Primeiro projeto git <br/>
-Calculadora web utilizando : Html , Css, BootStrap e Java Scritp
+Calculadora web utilizando : Html , Css, BootStrap e Java Scritp.
+
+Js
+Primeiro é necessário indentificar o que é ação e o que é valor , para isso será utilizado o evento de mouse onclick , oque for usado para calcular será a ação , e os numeros os valores ,
+
+Receberá dois parâmetros: tipo e valor
+onclick('acao',+') , 
+onclick('valor','7')
+
--- calc.js
+++ calc.js
@@ -3,7 +3,32 @@ function calcular (tipo, valor){
 
     if(tipo === 'acao'){
 
-    } else if(tipo === 'valor'){
+        if(valor === 'c'){
+            //limpar o visor
+            document.getElementById('resultado').value = ''
+        }
         
+        if(valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.' ){
+            document.getElementById('resultado').value += valor
+        }
+
+        if(valor === '='){
+            //Recuperando o resultado , processando usando eval e atributindo ao valor_campo
+            let valor_campo = eval ( document.getElementById('resultado').value)
+
+            //Sobrepondo
+            document.getElementById('resultado').value = valor_campo
+        }
+
+    } else if(tipo === 'valor'){
+
+        /*
+         recuperando o valor atual do campo 
+         var valor_campo =  document.getElementById('resultado').value .
+         Em vez de usar o trecho de código acima basta adicionar , no trecho abaixo  += no valor 
+        */
+
+        //selecionando o objeto e acessando o atributo value
+        document.getElementById('resultado').value += valor 
     }
 }
